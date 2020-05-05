fcaffa59b5217b634c72fb88288bc35350956aad
vermutsk <krnvitman@gmail.com>
2020-05-06 03:44:22 +1000

    Update menu2.py

--- lab3/menu2.py
+++ lab3/menu2.py
@@ -1,16 +1,15 @@
 choice1=int(input("Выберите метод шифровки:\n1) Метод замены\n2) Метод перестановки\n3) Метод гамирования\n4) Вернуться в главное меню\nВыбор: "))
-from  classes import Choice, Open_file
+from  classes import Choice, Open_files, Ways, Ways1
 choice=Choice(choice1)
 choice.crypt_choice()
+
 way=input("Введите путь к файлу текста: ")
-if way.endswith(".txt"):
-    text_filename=way
-else:
-    print("Неправильный формат файла")
+ways=Ways(way)
+ways.file_way()
+
 key=input("Введите путь к файлу ключа: ")
-if key.endswith(".key"):
-    key_filename=key
-else:
-    print("Неправильный формат файла")
-open_file=Open_file(key_filename)
-open_file.open_file()
+ways1=Ways1(key)
+ways1.file_way1()
+
+open_files=Open_files()
+open_files.open_file()
