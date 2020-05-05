033aedf083a92d124fb40ab49b3e64bee8de13b8
vermutsk <krnvitman@gmail.com>
2020-05-06 03:10:27 +1000

    Update menu2.py

--- lab3/menu2.py
+++ lab3/menu2.py
@@ -1,31 +1,17 @@
-try:
-    choice2=int(input("Выберите метод расшифровки:\n1) Метод замены\n2) Метод перестановки\n3) Метод гамирования\n4) Вернуться в главное меню\nВыбор: "))
-    if choice2==1:
-        crypt='шифр замены'
-    elif choice2==2:
-        crypt='шифр перестановки'
-    elif choice2==3:
-        crypt='шифр гамирования'    
-    else:
-        print("Ошибка")
-    way1=input("Введите путь к файлу шифротекста: ")
-    if way1.endswith(".encrypt"):
-        crypt_text_filename=way1
-    else:
-        print("Неправильный формат файла")
-    key=input("Введите путь к файлу ключа: ")
-    if key.endswith(".key"):
-        key_filename=key
-    else:
-        print("Неправильный формат файла")
-    with open(key_filename, "x", encoding="utf-8") as key_file:
-        key_list=[]
-        for line in key_file:
-            key_str=line.rstrip('\n')
-            key_list.append(key_str)
-            if key_list[0]==crypt:
-                print(crypt)
-            else:
-                print("Неправильный файл ключа")
-except Exception:
-    print("Выберите метод")
+choice1=int(input("Выберите метод шифровки:\n1) Метод замены\n2) Метод перестановки\n3) Метод гамирования\n4) Вернуться в главное меню\nВыбор: "))
+from  classes import Choice, Open_file
+choice=Choice(choice1, crypt)
+choice.crypt_choice()
+way=input("Введите путь к файлу текста: ")
+if way.endswith(".txt"):
+    text_filename=way
+else:
+    print("Неправильный формат файла")
+key=input("Введите путь к файлу ключа: ")
+if key.endswith(".key"):
+    key_filename=key
+
+else:
+    print("Неправильный формат файла")
+open_file=Open_file(key_filename)
+open_file.open_file()
