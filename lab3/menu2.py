c2ae6505ab219c0bd8451dec882b78b3a290062b
vermutsk <krnvitman@gmail.com>
2020-05-08 05:36:31 +1000

    Update menu2.py

--- lab3/menu2.py
+++ lab3/menu2.py
@@ -21,11 +21,35 @@ def key_open():
 flag=True
 while flag:
     print("Главное меню:")
-    choice=int(input("1) Зашифровать\n2) Расшифровать\n3) Сгенерировать ключ\nВыбор: ")) 
+    choice=int(input("1) Зашифровать\n2) Расшифровать\n\3) Сгенерировать ключ\nВыбор: ")) 
     if choice==1:
         choice1=int(input("Выберите метод шифровки:\n1) Метод замены\n2) Метод перестановки\n3) Метод гамирования\n4) Вернуться в главное меню\nВыбор: "))
         if choice1==1 or choice1==2 or choice1==3:
             file="txt"
+            fileway=input(f"Fileway for {file}:")
+            test()
+            print(fileway)
+            file="key"
+            fileway=input(f"Fileway for {file}:")
+            test()
+            if choice1==1:
+                crypt='шифр замены'
+                key_open()
+            elif choice1==2:
+                crypt='шифр перестановки'
+                key_open()
+            elif choice1==3:
+                crypt='шифр гамирования'
+                key_open()
+        elif choice1==4:
+            break
+        else:
+            raise Exception
+
+    elif choice==2:
+        choice1=int(input("Выберите метод расшифровки:\n1) Метод замены\n2) Метод перестановки\n3) Метод гамирования\n4) Вернуться в главное меню\nВыбор: "))
+        if choice1==1 or choice1==2 or choice1==3:
+            file="encode"
             fileway=input(f"Fileway for {file}:")
             test()
             file="key"
@@ -44,39 +68,6 @@ while flag:
             break
         else:
             raise Exception
-
-
-    elif choice==2:
-        choice2=int(input("Выберите метод расшифровки:\n1) Метод замены\n2) Метод перестановки\n3) Метод гамирования\n4) Вернуться в главное меню\nВыбор: "))
-        if choice2==1:
-            crypt='шифр замены'
-        elif choice2==2:
-            crypt='шифр перестановки'
-        elif choice2==3:
-            crypt='шифр гамирования'
-        elif choice2==4:
-            break    
-        else:
-            print("Ошибка")
-        way1=input("Введите путь к файлу шифротекста: ")
-        if way1.endswith(".encrypt"):
-            crypt_text_filename=way1
-        else:
-            print("Неправильный формат файла")
-        key=input("Введите путь к файлу ключа: ")
-        if key.endswith(".key"):
-            key_filename=key
-        else:
-            print("Неправильный формат файла")
-        with open(key_filename, "r", encoding="utf-8") as key_file:
-            key_list=[]
-            for line in key_file:
-                key_str=line.rstrip('\n')
-                key_list.append(key_str)
-                if key_list[0]==crypt:
-                    print(crypt)
-                else:
-                    print("Неправильный файл ключа")
 
 
     elif choice==3:
