6a800e3c8b6561f87912701dd6becc171c6a3e44
vermutsk <krnvitman@gmail.com>
2020-05-08 05:15:46 +1000

    Update classes.py

--- lab3/classes.py
+++ lab3/classes.py
@@ -1,20 +1,98 @@
-class Choice():
-    def __init__(self, choice1):
-        self.choice1=choice1
-    def crypt_choice(self, **args):
-        if self.choice1==1 or self.choice1==2 or self.choice1==3:
-            if self.choice1==1:
-                self.crypt='шифр замены'
-            elif self.choice1==2:
-                self.crypt='шифр перестановки'
-            elif self.choice1==3:
-                self.crypt='шифр гамирования'
-        else:
-            print(self.choice1, "Ошибка")
+from abc import ABC, abstractmethod
+
+class MyAbstractClass(ABC):
+    @abstractmethod
+    def encrypt(self,text_filename: str,key_filename: str, **args) -> bool:
+        '''
+        шифрование файла по ключу
+        text_filename -- имя файла для зашифрования
+        key_filename -- имя файла ключа
+        '''
+        raise Exception("Данный метод переопределяется в дочернем классе.\
+             Его логика уникальная для каждого отдельного класса.")
+
+    @abstractmethod
+    def decrypt(self,encrypted_filename: str,key_filename: str,**args) -> bool:
+        '''
+        расшифрование файла по ключу
+        encrypted_filename -- имя файла для расшифрования
+        key_filename -- имя файла ключа
+        '''
+        raise Exception("Данный метод переопределяется в дочернем классе.\
+             Его логика уникальная для каждого отдельного класса.")
+
+    @abstractmethod
+    def gen_key(self, **args) -> bool:
+        """генерация ключа"""
+        raise Exception("Данный метод переопределяется в дочернем классе.\
+             Его логика уникальная для каждого отдельного класса.")
+
+    def _read_text(self, text_filename: str) -> bool:
+        """общий метод для чтения текстового файла"""
+        raise Exception("Данный метод реализуется в этом классе.\
+                         Его логика не меняется для всех классов.")
+
+
+class GammEncrypt(MyAbstractClass):
+    def __init__(self):
+        pass
+
+    def encrypt(self,text_filename: str,key_filename: str,**args) -> bool:
+        pass
+
+    def decrypt(self,encrypted_filename: str,key_filename: str,**args) -> bool:
+        pass
+
+    def gen_key(self, **args) -> bool:
+        pass
+
+    def _read_encrypt(self, encrypted_filename: str) -> bool:
+        """прочитать защифрованный файл
+        encrypted_filename -- имя файла для чтения
+        """
+        pass
+
+    def _read_key(self, key_filename: str) -> bool:
+        """метод для чтения ключа
+        key_filename -- имя файла для чтения
+        """
+        pass
+
+
+class ReplaceEncrypt(MyAbstractClass):
+    def __init__(self):
+        pass
+
+    def encrypt(self, text_filename: str,key_filename: str,**args) -> bool:
+        pass
+
+    def decrypt(self, encrypted_filename: str, key_filename: str,**args) -> bool:
+        pass
+
+    def gen_key(self, **args) -> bool:
+        pass
+
+    def _read_encrypt(self, encrypted_filename: str) -> bool:
+        """прочитать защифрованный файл
+        encrypted_filename -- имя файла для чтения
+        """
+        pass
+
+    def _read_key(self, key_filename: str) -> bool:
+        """метод для чтения ключа
+        key_filename -- имя файла для чтения
+        """
+        pass
+
+
+
+
+
+
 class Ways():
     def __init__(self, way):
         self.way=way
-    def file_way(self, **args):
+    def file_way(self, *args):
         if self.way.endswith(".txt"):
             text_filename=self.way
             print(text_filename)
@@ -24,19 +102,19 @@ class Ways():
 class Ways1():
     def __init__(self, key):
         self.key=key
-    def file_way1(self, **args):
+    def file_way1(self, *args):
         if self.key.endswith(".key"):
             key_filename=self.key
             print(key_filename)
         else:
             print("Неправильный формат файла")
 
-class Open_files(Choice, Ways1):
+class Open_files(Ways1):
     key_list=[]
     def __init__(self, crypt, key_filename):
         self.key_filename=key_filename
         self.crypt=crypt
-    def open_file(self, **args):
+    def open_file(self, *args):
         with open(self.key_filename, "r", encoding="utf-8") as key_file:
             for line in key_file:
                 key_str=line.rstrip('\n')
