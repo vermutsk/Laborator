2e55a5c6ea773dbee429b3777bd5b58ce079a8d6
vermutsk <krnvitman@gmail.com>
2020-05-14 00:18:45 +1000

    Update menu2.py

--- lab3/menu2.py
+++ lab3/menu2.py
@@ -23,17 +23,32 @@ class AbstractClass(ABC):
 
 
     def _test_way(self, file):
-        flag1=True
-        while flag1:
+        flag=True
+        while flag:
             way=input(f"Введите путь к {file} файлу: ")
             try:
                 if way.endswith(f".{file}"):
-                    pass
+                    return way
                 else:
                     print("Неправильный тип файла")
-                return way 
             except Exception:
                 print("Ошибка в пути файла")
+            flag=False
+            break
+
+    def _create_file(self, file):
+        flag=True
+        while flag:
+            way=input(f"Введите путь для создания файла {file}: ")
+            try:    
+                if way.endswith(f".{file}"):
+                    return way
+                else:
+                    print("Неправильный тип файла")
+            except Exception:
+                print("Ошибка в пути файла")
+            flag=False
+            break
 
     def _read_text(self):
         text_list=self.text_open()
@@ -65,7 +80,7 @@ class AbstractClass(ABC):
             str0=list(line)
             encrypt_list.extend(str0)
             if len(encrypt_list)!=0:
-                return encrypt_list
+                return way
             else:
                 print("Пустой файл")
 
@@ -115,11 +130,7 @@ class ChangeEncrypt(AbstractClass):
         text_file=open(self._test_way('txt'), "r", encoding='utf-8')
         flag=True
         while flag:
-            way=input("Введите путь для создания файла шифротекста: ")
-            if way.endswith(".encrypt"):
-                encrypt_way=way
-            else:
-                print("Неправильный тип файла")
+            encrypt_way=self._create_file('encrypt')
             with open(encrypt_way,'w', encoding='utf-8') as encrypt_file:
                 encrypt_file.write('шифр замены\n')
                 str0=''
@@ -137,23 +148,44 @@ class ChangeEncrypt(AbstractClass):
                         str0=str0+str1[k]
                         k+=1
                     encrypt_file.write(str0)
-                    str0=''
-                    
-            flag==False 
-            print("Operation succesfull completed")
+                    str0=''        
+            flag=False
+            print(f"\nУспешно\nШифротекст был сохранен в файле: {encrypt_way}")
             break   
 
     def decrypt(self, **args):
+
+        key_dict=self._read_key()
+        encrypt_file=open(self._read_encrypt(), "r", encoding='utf-8')
+        flag=True
+        while flag:
+            text_way=self._create_file('txt')
+            with open(text_way,'w', encoding='utf-8') as text_file:
+                str0=''
+                for line in encrypt_file:
+                    str1=list(line)
+                    i=len(str1)
+                    k=0
+                    while k<i:  
+                        for key, value in key_dict.items():   
+                            if str1[k]==value:         
+                                str1[k]=key
+                                str0=str0+str1[k]
+                                str1[k]=''
+                                break
+                        str0=str0+str1[k]
+                        k+=1
+                    text_file.write(str0)
+                    str0=''        
+            flag=False
+            print(f"\nУспешно\nРасшифрованный текст был сохранен в файле: {text_way}")
+            break
         pass
 
     def gen_key(self, **args):
         flag1=True
         while flag1:
-            way=input("Введите путь для создания файла ключа: ")
-            if way.endswith(".key"):
-                key_fileway=way
-            else:
-                print("Неправильный тип файла")    
+            key_fileway=self._create_file('key')    
             self.alph_list=self.alp_open()
             key_list=random.sample(self.alph_list, len(self.alph_list))
             try:
@@ -165,14 +197,15 @@ class ChangeEncrypt(AbstractClass):
                         key_file.write(str0)
                         i+=1
                     flag1==False
+                    print("\nУспешно")
                     break 
             except FileExistsError:
                     print("Файл с таким именем уже существует")
 
 
     def _read_encrypt(self):
-        encrypt_filename=self.encrypt_open('шифр замены')
-        print(encrypt_filename)
+        encrypt_way=self.encrypt_open('шифр замены')
+        return(encrypt_way)
         
 
     def _read_key(self):
@@ -256,7 +289,7 @@ while flag:
             \n3) Сгенерировать ключ\n4) Выйти из программы\nВыбор: "))
         if choice==1:       
             while flag2:
-                choice1=int(input("\nВыберите метод шифровки:\n1) Метод замены\
+                choice1=int(input("\nВыберите метод шифровки:\n\n1) Метод замены\
                     \n2) Метод перестановки\n3) Метод гамирования\n4)Вернуться в главное меню\nВыбор: "))
                 if choice1!=1 and choice1!=2 and choice1!=3 and choice1!=4:
                     print("Ошибка ввода")
@@ -272,7 +305,7 @@ while flag:
 
         elif choice==2:     
             while flag2:
-                choice1=int(input("\nВыберите метод расшифровки:\n1) Метод замены\
+                choice1=int(input("\nВыберите метод расшифровки:\n\n1) Метод замены\
                     \n2) Метод перестановки\n3) Метод гамирования\n4)Вернуться в главное меню\nВыбор: "))
                 if choice1!=1 and choice1!=2 and choice1!=3 and choice1!=4:
                     print("Ошибка ввода")
@@ -288,7 +321,7 @@ while flag:
                 
         elif choice==3:     
             while flag2:
-                choice1=int(input("\nСгенерировать ключ для следующего алгоритма: \n1) Шифр замены\
+                choice1=int(input("\nСгенерировать ключ для следующего алгоритма: \n\n1) Шифр замены\
                     \n2) Шифр перестановки\n3) Шифр гамирования\n4) Вернуться в главное меню\nВыбор: "))
                 if choice1!=1 and choice1!=2 and choice1!=3 and choice1!=4:
                     print("Ошибка ввода")
