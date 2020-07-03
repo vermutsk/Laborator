b58e87dc8850feb650f962a7ed378a18e8838cae
vermutsk <krnvitman@gmail.com>
2020-07-03 14:13:34 +1000

    Update security.py

--- lab4/security.py
+++ lab4/security.py
@@ -7,7 +7,7 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Cipher._mode_eax import EaxMode
 _iv = b'help_me_please__'
 
-def _fill_random_bytes(bytes_str: bytes, length: int):
+def _fill_random_bytes(bytes_str: bytes, length: int)-> bytes:
     while len(bytes_str) % length != 0: 
         symbol = bytes()
         if len(bytes_str) > 0:
@@ -16,44 +16,45 @@ def _fill_random_bytes(bytes_str: bytes, length: int):
         bytes_str += symbol
     return bytes_str
 
-def publick():
+def publick()->bytes:
     with open('rsa_public.pem','r',encoding='utf-8') as pub_key:
         file=pub_key.read()
     file1=file.encode("utf-8")
     return file1
 
-def private():
+def private()->bytes:
     with open('private_rsa_key.bin','r',encoding='utf-8') as priv_key:
         file=priv_key.read()
     file1=file.encode("utf-8")
     return file1
 
-def publick_ordinary(way:str):
+def publick_ordinary(way:str)->bytes:
     way_new=os.path.join(way,"rsa_public.pem")
     with open(way_new,'r',encoding='utf-8') as pub_key:
         file=pub_key.read()
     file1=file.encode("utf-8")
     return file1
 
-def private_ordinary(way:str):
+def private_ordinary(way:str)->bytes:
     way_new=os.path.join(way, "private_rsa_key.bin")
     with open(way_new,'r',encoding='utf-8') as priv_key:
         file=priv_key.read()
     file1=file.encode("utf-8")
     return file1
 
-def check_password(hashed_password:str, user_password:str):
+def check_password(hashed_password:str, user_password:str)->bool:
     password, salt = hashed_password.split(':')
     return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
 
-def hash_password(password:str):
+def hash_password(password:str)->str:
     salt ="eyafjallajokulll"
     salt_b=b"eyafjallajokulll"
     return hashlib.sha256(salt_b + password.encode('utf-8')).hexdigest() + ':' + salt
 
-def generate_keys(way:str):
+def generate_keys(way:str)->None:
+    print("Генерация ключей...")
     code = 'helphelphe'
-    key = RSA.generate(1024)
+    key = RSA.generate(4096)
 
     encrypted_key = key.exportKey(
         passphrase=code, 
@@ -69,7 +70,7 @@ def generate_keys(way:str):
         file_key.write(encrypted_key)
     security_sys_files(os.path.join(way,'private_rsa_key.bin')) 
 
-def security_files(way:str,name_file:str):
+def security_files(way:str,name_file:str)->None:
     data: bytes=b''
     with open(name_file,'rb') as some_file:
         data=some_file.read()
@@ -92,7 +93,7 @@ def security_files(way:str,name_file:str):
         
         out_file.write(ciphertext)
 
-def security_sys_files(name_file:str):
+def security_sys_files(name_file:str)->None:
     data:bytes=b''
     with open(name_file,'rb') as some_file:
         data=some_file.read()
@@ -105,15 +106,17 @@ def security_sys_files(name_file:str):
    
         cipher_rsa = PKCS1_OAEP.new(recipient_key)
         out_file.write(cipher_rsa.encrypt(session_key))
-
-        cipher_aes: EaxMode = AES.new(session_key, AES.MODE_EAX)
-        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-   
-        out_file.write(cipher_aes.nonce)
-        out_file.write(tag)
+        
+        len_block = len(data).to_bytes(length=16, byteorder='big')
+        data = len_block + data
+        data = _fill_random_bytes(data, 16)
+        
+        cipher_aes = AES.new(session_key, AES.MODE_CBC, _iv)
+        ciphertext = cipher_aes.encrypt(data)
+        
         out_file.write(ciphertext)
 
-def decode_files(way:str,name_file:str):
+def decode_files(way:str,name_file:str)->None:
     code = 'helphelphe'
     with open(name_file, 'rb') as fobj:
         private_key = RSA.import_key(
@@ -128,6 +131,7 @@ def decode_files(way:str,name_file:str):
         session_key = cipher_rsa.decrypt(enc_session_key)  
         
         cipher_aes = AES.new(session_key, AES.MODE_CBC, _iv)
+
         data = cipher_aes.decrypt(ciphertext)
         size = int.from_bytes(data[:16], byteorder='big')
         data = data[16:size + 16]
@@ -137,7 +141,7 @@ def decode_files(way:str,name_file:str):
 
 
 
-def decode_sys_files(name_file:str):
+def decode_sys_files(name_file:str)->None:
     code = 'helphelphe'
     with open(name_file, 'rb') as fobj:
         private_key = RSA.import_key(
@@ -145,15 +149,19 @@ def decode_sys_files(name_file:str):
              passphrase=code
          )
         
-        enc_session_key, nonce, tag, ciphertext = [
-            fobj.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1)
+        enc_session_key, ciphertext = [
+            fobj.read(x) for x in (private_key.size_in_bytes(), -1)
         ]
       
         cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_session_key)
+        session_key = cipher_rsa.decrypt(enc_session_key)  
+        
+        cipher_aes = AES.new(session_key, AES.MODE_CBC, _iv)
 
-        cipher_aes: EaxMode = AES.new(session_key, AES.MODE_EAX,nonce)
-        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+        data = cipher_aes.decrypt(ciphertext)
+        size = int.from_bytes(data[:16], byteorder='big')
+        data = data[16:size + 16]
+    
     with open(name_file,'wb') as out_file:
         out_file.write(data)
 
