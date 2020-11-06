1530661ced0792fe074e55379c449e5355dd3054
vermutsk <krnvitman@gmail.com>
2020-11-06 20:34:16 +1000

    Update config.py

--- lab10/config.py
+++ lab10/config.py
@@ -14,33 +14,33 @@ class Config():
 
     def __init__(self):
         self.data = ''
-        self.APP_ID = ''
-        self.SECRET = ''
-        self.ACS_TO = ''
-        with open('app_id.txt', 'r') as app:
-            list0 = []
-            for line in app:
-                str0 = line.rstrip('\n')
-                list0.append(str0)
-            self.APP_ID = list0[0]
-            self.SECRET = list0[1]
-            self.ACS_TOK = list0[2]
+        self.APP_ID = open('app_id.txt', 'r').read()
+        #self.SECRET = ''
+        #self.ACS_TO = ''
+        #with open('app_id.txt', 'r') as app:
+        #    list0 = []
+        #    for line in app:
+        #        str0 = line.rstrip('\n')
+        #        list0.append(str0)
+        #    self.APP_ID = list0[0]
+        #    self.SECRET = list0[1]
+        #    self.ACS_TOK = list0[2]
 
     def is_loaded(self):
-        
+    
         if os.path.isfile('id_token.txt') is True:
             self.data = open('id_token.txt', 'r').read()
         else:
             self.data= self.new_token()
             with open('id_token.txt', 'a') as doc:
                 doc.write(self.data)
-        check = BASE_URL + f'secure.checkToken?access_token={self.ACS_TOK}&client_secret={self.SECRET}&v=5.21&client_id={self.APP_ID}&token={self.data}'
-        load = requests.get(check).json()
-        print(load)
-        if load['response']['success'] != 1:
-            return False
-        else:
-            return True
+        #check = BASE_URL + f'secure.checkToken?access_token={self.ACS_TOK}&client_secret={self.SECRET}&v=5.21&client_id={self.APP_ID}&token={self.data}'
+        #load = requests.get(check).json()
+        #print(load)
+        #if load['response']['success'] != 1:
+        #    return False
+        #else:
+        #    return True
     
     def new_token(self):
         template = re.compile(r'^https://oauth.vk.com/blank.html#access_token=(\w+)&expires_in=(\d+)&user_id=(\d+)$')
