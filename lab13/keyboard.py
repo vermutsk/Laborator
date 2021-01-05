3e5a05132cc1afa7305cc07aa69e04c86b2a81a9
vermutsk <krnvitman@gmail.com>
2021-01-05 21:49:11 +1000

    Update keyboard.py

--- lab13/keyboard.py
+++ lab13/keyboard.py
@@ -1,6 +1,14 @@
 from aiogram import Bot, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
+butt_fname =  KeyboardButton('Фамилия')
+butt_name = KeyboardButton('Имя')
+butt_oname = KeyboardButton('Отчество')
+butt_dolj = KeyboardButton('Должность')
+butt_room = KeyboardButton('Кабинет')
+butt_phone = KeyboardButton('Телефон')
+butt_mail =  KeyboardButton('Email')
 butt_full = KeyboardButton('Полная')
 butt_fio = KeyboardButton('Фио')
 butt_new = KeyboardButton('Создать')
@@ -9,3 +17,4 @@ butt_delete = KeyboardButton('Удалить')
 butt_save = KeyboardButton('Сохранить')
 board_1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(butt_full, butt_fio)
 board_3 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(butt_new, butt_change, butt_delete).add(butt_save)
+board_5 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(butt_dolj).row(butt_fname, butt_name, butt_oname).row(butt_room, butt_phone, butt_mail)
