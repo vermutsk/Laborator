1e5bd8325a37efa236a2bdf8af58719d3670709a
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-04-01 17:46:32 +1000

    Create crypt_lab8.py

---
+++ NE_metodi/crypt_lab8.py
@@ -0,0 +1,47 @@
+from Crypto.Util import number
+import SHA
+import random
+
+def gen_alpha(a, b):
+    #генерация образующего элемента
+    if a < b:
+        return gen_alpha(b, a)
+    elif a % b == 0:
+        return b;
+    else:
+        return gen_alpha(b, a % b)
+
+def gen_keys():
+    p = number.getPrime(256, None)
+    alpha = random.randint(pow(10, 20), p)
+    while gen_alpha(p, alpha) != 1:
+        alpha = random.randint(pow(10, 20), p)
+    a = random.randint(1, p-2)
+    beta = pow(alpha, a, p)
+    
+    with open("privat.txt","w",encoding="UTF-8") as prifile:
+        prifile.write(str(a))
+    with open("publick.txt","w",encoding="UTF-8") as pubfile:
+        pubfile.write(str(alpha)+'\n'+str(beta)+'\n'+str(p))
+
+def gamal():
+    mes = 'hello'
+    hesh = int(SHA.sha256(mes), base=16)
+    with open("privat.txt","r") as prifile:
+        a = int(prifile.read())
+    with open("publick.txt","r") as pubfile:
+        alpha, beta, p = pubfile.read().split('\n')
+    alpha, beta, p = int(alpha), int(beta), int(p)
+    r = random.randint(1, p-2)
+    while gen_alpha(r, p-1)!=1:
+        r = random.randint(1, p-2)
+    gamma = alpha*r%p
+    delta = ((hesh-alpha*gamma)*r-1)%(p-1)
+    if pow(beta, gamma)*pow(gamma, delta)==alpha*hesh%p:
+        print("true")
+    else:
+        print("lol")
+    
+
+gen_keys()
+gamal()
