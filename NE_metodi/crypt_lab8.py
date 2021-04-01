1650aa9d60115e4006232f15edf3df59cd425097
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-04-01 17:51:30 +1000

    Update crypt_lab8.py

--- NE_metodi/crypt_lab8.py
+++ NE_metodi/crypt_lab8.py
@@ -24,6 +24,16 @@ def gen_keys():
     with open("publick.txt","w",encoding="UTF-8") as pubfile:
         pubfile.write(str(alpha)+'\n'+str(beta)+'\n'+str(p))
 
+def power(a, b, c):
+    x = 1
+    y = a
+    while b > 0:
+        if b % 2 == 0:
+            x = (x * y) % c;
+        y = (y * y) % c
+        b = int(b / 2)
+    return x % c
+
 def gamal():
     mes = 'hello'
     hesh = int(SHA.sha256(mes), base=16)
