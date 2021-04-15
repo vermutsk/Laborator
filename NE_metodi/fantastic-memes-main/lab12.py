8fa55fdfeabae97069d3f5e6dbf3d7d07a7d10f5
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-04-15 23:14:05 +1000

    Create lab12.py

---
+++ NE_metodi/fantastic-memes-main/lab12.py
@@ -0,0 +1,100 @@
+import pyAesCrypt
+import os
+import random
+import time
+
+
+def remove_file(file: str):
+    os.remove(file)
+
+
+def AES_encrypt(file_txt: str, key: str, buffsize=512 * 1024):
+    # 1. Файл который шифруется,
+    # 2. Ключ по которому шифруют,
+    # 3. (Стандартное значение 512 * 1024) Буфер
+    file_aes = file_txt + '.aes'  # Зашифрованный файл
+    pyAesCrypt.encryptFile(file_txt, file_aes, key, buffsize)
+    return file_aes
+
+
+def AES_decrypt(file_aes: str, file_txt: str, key: str, buffsize=512 * 1024):
+    # 1. Файл который рашифровываеться,
+    # 2. Файл куда записываеться расшифрованный текст
+    # 3. Ключ по которому расшифровывают,
+    # 4. (Стандартное значение 512 * 1024) Буффер
+    pyAesCrypt.decryptFile(file_aes, file_txt, key, buffsize)
+
+
+def gen_key(size):
+    # 1. Размер ключа в битах
+    key = random.getrandbits(size)
+    key = bin(key)[2:]
+    return key
+
+
+def timestamp():
+    stamp = str(time.time())
+    stamp = stamp.split('.')
+    stamp = ''.join(x for x in stamp)
+    return stamp
+
+
+def step_1(User_A):
+    with open("File_A.txt","w") as file:
+        file.write(str(User_A))
+
+
+def step_2(User_B,User_B_id):
+    with open("File_A.txt","r") as file:
+        rA=file.readline()
+    os.remove("File_A.txt")
+    
+    # b -> a 
+    with open("File_B.txt","w") as file:
+        strin=rA+"\n"+str(User_B)+"\n"+User_B_id
+        file.write(strin)
+    aes_file=AES_encrypt("File_B.txt",key)
+    os.remove("File_B.txt")
+    return aes_file
+
+
+def step_3(User_A,User_B_id,aes_file):
+    AES_decrypt(aes_file,"File_A_From_B.txt",key)
+    strin=list()
+    with open("File_A_From_B.txt","r") as file:
+        strin=file.read()
+    strin=strin.split("\n")
+    os.remove(aes_file)
+    os.remove("File_A_From_B.txt")
+    
+    if User_A==int(strin[0]) and User_B_id==strin[2]:
+        print("User A: the user B identity is verified\n")
+    with open("File_B_From_A.txt","w") as file:
+        file.write(strin[1]+"\n"+strin[0]+"\n"+User_A_id)
+    aes_file=AES_encrypt("File_B_From_A.txt",key)
+    os.remove("File_B_From_A.txt")
+    return aes_file
+
+
+def step_4 (User_B,User_A_id,aes_file):
+    #step 4 for B
+    AES_decrypt(aes_file,"File_B_From_A_From_B.txt",key)
+    os.remove(aes_file)
+    with open("File_B_From_A_From_B.txt","r") as file:
+        strin=file.read()
+    strin=strin.split("\n")
+    if User_B==int(strin[0]) and User_A_id==strin[2]:
+        print("User B: the user B identity is verified\n")
+    os.remove("File_B_From_A_From_B.txt")
+
+
+key = gen_key(256)
+User_A=random.randint(5,1000000000000000000000000000000000000000000000000000000000000000000)
+User_B=random.randint(5,1000000000000000000000000000000000000000000000000000000000000000000)
+User_A_id="id_001"
+User_B_id="id_002"
+
+step_1(User_A)
+aes_file=step_2(User_B,User_B_id)
+aes_file=step_3(User_A,User_B_id,aes_file)
+step_4(User_B,User_A_id,aes_file)
