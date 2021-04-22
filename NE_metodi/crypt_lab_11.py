e418b32b38f8aa04da0b35edac31697d19e1c457
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-04-22 22:34:12 +1000

    Update crypt_lab_11.py

--- NE_metodi/crypt_lab_11.py
+++ NE_metodi/crypt_lab_11.py
@@ -1,134 +1,92 @@
-import random
+
+import pyAesCrypt
 import os
-import BASE
-import math
-import json
-
-globkey = b'V%\xb84\x95\x88m\xa2\x9a\x1e\xb7F|\xd6\x990'
+import random
+from datetime import datetime
 
 
-# def filer(way):
-#     with open(way, "r") as f:
-#         varbls = {line[0]: line[3:] for line in f}
-#     return varbls
+def remove_file(file: str):
+    os.remove(file)
 
 
-def LogIn():
-    way = 'logpas.txt'
-    if os.path.isfile(way):
-        with open(way, 'r') as file:
-            UsersInformation = json.load(file)
-            # UserInformation = file.read()
+def AES_encrypt(file_txt: str, key: str, buffsize=512 * 1024):
+    file_aes = file_txt + '.aes'  # Зашифрованный файл
+    pyAesCrypt.encryptFile(file_txt, file_aes, key, buffsize)
+    return file_aes
+
+
+def AES_decrypt(file_aes: str, file_txt: str, key: str, buffsize=512 * 1024):
+    pyAesCrypt.decryptFile(file_aes, file_txt, key, buffsize)
+
+
+def gen_key(size):
+    key = random.getrandbits(size)
+    key = bin(key)[2:]
+    return key
+
+
+def step_1(marker, User_A):
+    with open("File_A.txt","x") as file:
+        strin=marker + "\n" + str(User_A)
+        file.write(strin)
+    aes_file=AES_encrypt("File_A.txt",key)
+    os.remove("File_A.txt")
+    return aes_file
+
+
+def step_2(User_A,marker,aes_file):
+    AES_decrypt(aes_file,"File_B_From_A.txt",key)
+    strin=list()
+    with open("File_B_From_A.txt","r") as file:
+        strin=file.read()
+    strin=strin.split("\n")
+    os.remove(aes_file)
+    os.remove("File_B_From_A.txt")
+    if User_A==strin[1] and marker==strin[0]:
+        print("User B: the user A identity is verified\n")
+    with open("File_A_From_B.txt","w") as file:
+        file.write(strin[0]+"\n"+User_B)
+    aes_file=AES_encrypt("File_A_From_B.txt",key)
+    os.remove("File_A_From_B.txt")
+    return aes_file
+
+
+def step_3 (User_B,marker,aes_file):
+    #step 4 for B
+    AES_decrypt(aes_file,"File_A_From_B_From_A.txt",key)
+    os.remove(aes_file)
+    with open("File_A_From_B_From_A.txt","r") as file:
+        strin=file.read()
+    strin=strin.split("\n")
+    if User_B==strin[1] and marker==strin[0]:
+        print("User A: the user B identity is verified\n")
+    os.remove("File_A_From_B_From_A.txt")
+
+
+key = gen_key(256)
+
+User_A="id_001"
+User_B="id_002"
+
+
+while True:
+    print('>>Выберите идентификатор<<: \n1. Метка врeмени \n2. Случайное число \n3. Выход')
+    choose = input('>>')
+    if choose == '1':
+        marker = datetime.now()
+        marker = str(marker)
+        marker = marker.replace(':', '').replace('-', '').replace(' ', '')[2:].split('.')[0]
+        print("Метка создана")
+    elif choose == "2":
+        marker=str(random.randint(5,pow(2,32)))
+        print("Число создано")
+    elif choose == '3':
+        break
     else:
-        UsersInformation = {}
-    f = True
-    while f == True:
-        print('Login:')
-        login = input('>>')
-        if login in UsersInformation.keys():
-            print('Тhis login is busy')
-            return 0
-        f = False
-    print('Password:')
-    password = input('>>')
-    password_base, rav = BASE.Text_to_ASCII_64(password)
-    H = BASE.encrypted_BASE64(password_base, rav)
-    UsersInformation.update({login: H})
-    with open(way, 'w') as file:
-        json.dump(UsersInformation, file)
-
-
-def SignIn():
-    way = 'logpas.txt'
-    if os.path.isfile(way):
-        with open(way, 'r') as file:
-            UsersInformation = json.load(file)
-    else:
-        UsersInformation = {}
-        print('No accounts')
-        return 0
-    while True:
-        print('Login:')
-        login = input('>>')
-        if login in UsersInformation.keys():
-            print('Password:')
-            password = input('>>')
-            password_base, rav = BASE.Text_to_ASCII_64(password)
-            H = BASE.encrypted_BASE64(password_base, rav)
-            if H != UsersInformation[login]:
-                print('Logins do not match')
-                return 0, 0
-            else:
-                print('Logged in')
-                return login, H
-        else:
-            print('Аccount does not exist')
-            return 0, 0
-
-
-def check_password(login, password_b):
-    way = 'logpas.txt'
-    if os.path.isfile(way):
-        with open(way, 'r') as file:
-            UsersInformation = json.load(file)
-    else:
-        print('No accounts')
-        return False
-    if login in UsersInformation.keys() and UsersInformation[login] == password_b:
-        return True
-    else:
-        return False
-
-
-def choose():
-    while True:
-        print('\nВыберите действие:')
-        print('1.Log In\n2.Sign In\n3.Exit')
-        ch = input('Select action number: ')
-        if ch == '1.' or ch == '1':
-            if LogIn() == 0:
-                print("Try again")
-        elif ch == '2.' or ch == '2':
-            login, paswword_b = SignIn()
-            return login, paswword_b
-        elif ch == '3.' or ch == '3':
-            print('...EXIT...')
-            exit()
-        else:
-            print('There is no such option')
-
-
-def A_to_B():
-    login, paswword_b = choose()
-
-
-if __name__ == '__main__':
-    flag = False
-    while True:
-        print("Вход пользователя А:")
-        login_A, password_A = choose()
-        ###
-        print("Пользователь В индентифицирует пользователя А")
-        if check_password(login_A, password_A):
-            print("Пользователь А индетифицирован")
-            ###
-            while True:
-                print("Вход полльзователя В")
-                login_B, password_B = choose()
-                ###
-                print("Пользователь A индентифицирует пользователя B")
-                if check_password(login_B, password_B):
-                    print("Пользователь B индетифицирован")
-                    print("Соединение устоновленно!")
-                    flag = True
-                    break
-                else:
-                    print("Пользователь B клоун")
-                    print("Try again")
-                    continue
-            if flag:
-                break
-        else:
-            print("Пользователь А клоун")
-            print("Try again")
-            continue
+        print("Неправильно введена команда\n")
+        continue
+    aes_file=step_1(marker, User_A)
+    aes_file=step_2(User_A,marker,aes_file)
+    step_3(User_B,marker,aes_file)
+    break
+    
