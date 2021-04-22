8b37b6ec0dce3f8629f62511f00f26f76728b130
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-04-22 22:34:15 +1000

    Update crypt_lab_12.py

--- NE_metodi/crypt_lab_12.py
+++ NE_metodi/crypt_lab_12.py
@@ -1,131 +1,84 @@
-import AES
-import json
-from datetime import datetime
+import pyAesCrypt
+import os
 import random
+import time
 
 
-def AccountCreation():
-    def creat_acc(name_acc: str, file: str):
-        print('>>>Регистрация пользователя {}'.format(name_acc))
-        while True:
-            loginA = input('Введите логин: ')
-            passwordA = input('Введите пароль: ')
-            passwordA_chek = input('повторите пароль: ')
-            if passwordA_chek == passwordA:
-                with open(file, 'w') as f:
-                    UsersInformation = {loginA: passwordA}
-                    json.dump(UsersInformation, f)
-                AES.AES_encrypt(file, generalkey)
-                print('Аккаунт {} создан'.format(name_acc))
-                break
-
-    A_file = open('User_A.txt', 'w')
-    A_file.close()
-    B_file = open('User_B.txt', 'w')
-    B_file.close()
-
-    with open('aes_key.txt', "w", encoding="UTF-8") as keyfile:
-        generalkey = AES.gen_key(16)
-        keyfile.write(str(generalkey))
-
-    creat_acc('A', 'User_A.txt')
-    creat_acc('B', 'User_B.txt')
+def remove_file(file: str):
+    os.remove(file)
 
 
-def AccountLogIn(userletter):
-    print('>>>Вход пользователя', userletter)
-    with open('aes_key.txt', "r", encoding="UTF-8") as keyfile:
-        generalkey = keyfile.read()
-    login = input('Введите логин: ')
-    password = input('Введите пароль: ')
-    UsersInformation_check = {login: password}
-    way = 'User_' + userletter + '.txt.aes'
-    new_file_name = AES.AES_decrypt(way, way.rstrip('.aes'), generalkey)
-    with open(new_file_name, 'r') as file:
-        UsersInformation = json.load(file)
-    if UsersInformation_check == UsersInformation:
-        print('Вход выполнен')
-    else:
-        print('Неверный логин или пароль')
-        return 0
+def AES_encrypt(file_txt: str, key: str, buffsize=512 * 1024):
+    file_aes = file_txt + '.aes'  # Зашифрованный файл
+    pyAesCrypt.encryptFile(file_txt, file_aes, key, buffsize)
+    return file_aes
 
 
-def Sending(sender, marker):
-    with open('aes_key.txt', "r", encoding="UTF-8") as keyfile:
-        generalkey = keyfile.read()
-    # получение расшифрованного логина/пароля
-    # way = 'User_' + sender + '.aes'
-    # new_file_name = 'Message_From_' + AES.AES_decrypt(way, generalkey)
-    # with open(new_file_name, 'r') as file:
-    # UsersInformation = json.load(file)
-    # шифрование маркера
-    mark_way = 'Marker_' + sender + '.txt'
-    with open(mark_way, 'w') as file:
-        json.dump(marker, file)
-    AES.AES_encrypt(mark_way, generalkey)
+def AES_decrypt(file_aes: str, file_txt: str, key: str, buffsize=512 * 1024):
+    pyAesCrypt.decryptFile(file_aes, file_txt, key, buffsize)
 
 
-def Receive(sender, recipient):
-    with open('aes_key.txt', "r", encoding="UTF-8") as keyfile:
-        generalkey = keyfile.read()
-    # расшифровывание маркера
-    mark_way = 'Marker_' + sender + '.txt.aes'
-    new_markfile_name = AES.AES_decrypt(mark_way, mark_way.rstrip('.aes'), generalkey)
-    with open(new_markfile_name, 'r') as file:
-        marker = json.load(file)
-    # подтверждение отправителя
-    way = 'User_' + sender + '.txt.aes'
-    new_file_name = 'Message_From_' + way.rstrip('.aes')
-    AES.AES_decrypt(way, new_file_name, generalkey)
-    with open(new_file_name, 'r') as file:
-        In_message = json.load(file)
-    correct_way = 'User_' + sender + '.txt'
-    with open(correct_way, 'r') as file:
-        Correct = json.load(file)
-    if In_message == Correct:
-        print(f'Пользователь {recipient} подтвердил личность пользователя {sender}')
-    else:
-        print('Ошибка аутентификации')
-        return 0
-    return marker
+def gen_key(size):
+    key = random.getrandbits(size)
+    key = bin(key)[2:]
+    return key
 
 
-def MainMemu():
-    while True:
-        print('>>Главное меню<<\n1.Регистрация \n2.Вход \n3.Обмен сообщениями \n4.Выход')
-        choose = input('>>')
-        if choose == '1':
-            AccountCreation()
-        elif choose == '2':
-            AccountLogIn('A')
-            AccountLogIn('B')
-        elif choose == '3':
-            while True:
-                print('>>Выберите идентификатор<<: \n1. Метка врмени \n2. Случайное число')
-                mark_ch = input('>>')
-                if mark_ch == '1':
-                    marker = datetime.now()
-                    marker = str(marker)
-                    marker = marker.replace(':', '').replace('-', '').replace(' ', '')[2:].split('.')[0]
-                    print("Метка создана")
-                elif mark_ch == "2":
-                    marker = random.randint(1, 9876543210)
-                    print("Число создано")
-                else:
-                    print("Неправильно введена команда\n")
-                    continue
-                Sending('A', marker)
-                marker = Receive('A', 'B')
-                Sending('B', marker)
-                marker = Receive('B', 'A')
-                Sending('A', marker)
-                marker = Receive('A', 'B')
-                break
-        elif choose == '4':
-            break
-        else:
-            print("Неправильно введена команда\n")
+def step_1(User_A):
+    with open("File_A.txt","w") as file:
+        file.write(str(User_A))
 
 
-if __name__ == '__main__':
-    MainMemu()
+def step_2(User_B,User_B_id):
+    with open("File_A.txt","r") as file:
+        rA=file.readline()
+    os.remove("File_A.txt")
+    # b -> a 
+    with open("File_B.txt","w") as file:
+        strin=rA+"\n"+str(User_B)+"\n"+User_B_id
+        file.write(strin)
+    aes_file=AES_encrypt("File_B.txt",key)
+    os.remove("File_B.txt")
+    return aes_file
+
+
+def step_3(User_A,User_B_id,aes_file):
+    AES_decrypt(aes_file,"File_A_From_B.txt",key)
+    strin=list()
+    with open("File_A_From_B.txt","r") as file:
+        strin=file.read()
+    strin=strin.split("\n")
+    os.remove(aes_file)
+    os.remove("File_A_From_B.txt")
+    
+    if User_A==int(strin[0]) and User_B_id==strin[2]:
+        print("User A: the user B identity is verified\n")
+    with open("File_B_From_A.txt","w") as file:
+        file.write(strin[1]+"\n"+strin[0]+"\n"+User_A_id)
+    aes_file=AES_encrypt("File_B_From_A.txt",key)
+    os.remove("File_B_From_A.txt")
+    return aes_file
+
+
+def step_4 (User_B,User_A_id,aes_file):
+    #step 4 for B
+    AES_decrypt(aes_file,"File_B_From_A_From_B.txt",key)
+    os.remove(aes_file)
+    with open("File_B_From_A_From_B.txt","r") as file:
+        strin=file.read()
+    strin=strin.split("\n")
+    if User_B==int(strin[0]) and User_A_id==strin[2]:
+        print("User B: the user B identity is verified\n")
+    os.remove("File_B_From_A_From_B.txt")
+
+
+key = gen_key(256)
+User_A=random.randint(5,pow(2,32))
+User_B=random.randint(5,pow(2,32))
+User_A_id="id_001"
+User_B_id="id_002"
+
+step_1(User_A)
+aes_file=step_2(User_B,User_B_id)
+aes_file=step_3(User_A,User_B_id,aes_file)
+step_4(User_B,User_A_id,aes_file)
